264e03006ec04ec993263d2839d5d047c35a83b1
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-06-15 15:54:55 +0900

    fix:prismaは現在turbopackに対応していないため、webpackに変更
    svgrをコンポーネントとして扱う際の設定をturbopackからwebpackに修正。

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
-    },
+  // svgrをコンポーネントとして扱う
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      exclude: /node_modules/,
+      loader: "@svgr/webpack",
+    });
+    return config;
   },
 };
 
